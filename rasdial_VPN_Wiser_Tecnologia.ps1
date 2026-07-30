1d08269ecb8b4b283f9721f9276e885617478b93
Demétrio de Freitas Oliveira <81397961+dmtfreitas@users.noreply.github.com>
2026-07-30 17:10:31 -0300

    Update rasdial_VPN_Wiser_Tecnologia.ps1

--- rasdial_VPN_Wiser_Tecnologia.ps1
+++ rasdial_VPN_Wiser_Tecnologia.ps1
@@ -1,39 +1,64 @@
 
 # rasdial_VPN_Wiser_Tecnologia.ps1
 
-$name = 'VPN_Wiser_Tecnologia'
-$user = ''
-$password = ''
+$name = "VPN_Wiser_Tecnologia"
+$user = ""
+$password = ""
 
-$vpn = Get-VpnConnection -Name $name -ErrorAction SilentlyContinue
+function Test-VpnConnected {
+
+    param([string]$VpnName)
+
+    $connection = Get-VpnConnection -Name $VpnName -ErrorAction SilentlyContinue
+
+    if (-not $connection) {
+        $connection = Get-VpnConnection -Name $VpnName -AllUserConnection -ErrorAction SilentlyContinue
+    }
+
+    if ($connection -and $connection.ConnectionStatus -eq "Connected") {
+        return $true
+    }
+
+    $rasdial_output = rasdial.exe 2> $null
+
+    if ($rasdial_output -match [regex]::Escape($VpnName)) {
+        return $true
+    }
+
+    return $false
+}
+
+$exists_connection = Get-VpnConnection -Name $name -ErrorAction SilentlyContinue
+
+if (-not $exists_connection) {
+    $exists_connection = Get-VpnConnection -Name $name -AllUserConnection -ErrorAction SilentlyContinue
+}
+
+if (-not $exists_connection) {
+    Write-Host "VPN $name not found!" -ForegroundColor DarkRed
+    Exit
+}
 
 while ($true) {
 
-    if (-not $vpn) {
+    $timestamp = Get-Date -Format "dd/MM/yyyy HH:mm:ss"
 
-        Write-Host 'VPN VPN_Wiser_Tecnologia not found!' -ForegroundColor DarkRed -ErrorAction SilentlyContinue
-        Exit
-
-    }
-
-    if ($vpn.ConnectionStatus -eq 'Connected') {
-        Write-Host 'VPN VPN_Wiser_Tecnologia is connected!' -ForegroundColor DarkCyan -ErrorAction SilentlyContinue
+    if (Test-VpnConnected -VpnName $name) {
+        Write-Host "[$timestamp] VPN $name is connected!" -ForegroundColor DarkCyan
     } else {
 
-        Write-Host 'Connecting to the VPN VPN_Wiser_Tecnologia' -ForegroundColor Yellow -ErrorAction SilentlyContinue
+        Write-Host "[$timestamp] Connecting to the VPN $name" -ForegroundColor Yellow
 
         rasdial.exe $name $user $password | Out-Null
 
-        $vpn = Get-VpnConnection -Name $name -ErrorAction SilentlyContinue
+        Start-Sleep -Seconds 10.5
 
-        if ($vpn.ConnectionStatus -eq 'Connected') { 
-            Write-Host 'VPN VPN_Wiser_Tecnologia is connected!' -ForegroundColor Green -ErrorAction SilentlyContinue
+        if (Test-VpnConnected -VpnName $name) {
+            Write-Host "[$timestamp] VPN $name is connected!" -ForegroundColor Green
         } else {
-            Write-Host 'Unable to connect to the VPN VPN_Wiser_Tecnologia' -ForegroundColor Red -ErrorAction SilentlyContinue
+            Write-Host "[$timestamp] Unable to connect to the VPN $name" -ForegroundColor Red
         }
-
     }
 
-    Start-Sleep -Seconds 60 -ErrorAction SilentlyContinue
-
+    Start-Sleep -Seconds 60.5
 }
