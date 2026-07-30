ab53559d6bb6ebd06edd695e4141ad50df10a5f3
Demétrio de Freitas Oliveira <81397961+dmtfreitas@users.noreply.github.com>
2026-07-30 11:14:05 -0300

    Add files via upload

---
+++ rasdial_VPN_Wiser_Tecnologia.ps1
@@ -0,0 +1,37 @@
+﻿
+# rasdial_VPN_Wiser_Tecnologia.ps1
+
+$name = 'VPN_Wiser_Tecnologia'
+$user = 'paulo.licciardi'
+$password = '0404@Wi'
+
+$vpn = Get-VpnConnection -Name $name -ErrorAction SilentlyContinue
+
+while ($true) {
+
+    if (-not $vpn) {
+
+        Write-Host 'VPN VPN_Wiser_Tecnologia not found!' -ForegroundColor DarkRed -ErrorAction SilentlyContinue
+        Exit
+
+    }
+
+    if ($vpn.ConnectionStatus -eq 'Connected') {
+        Write-Host 'VPN VPN_Wiser_Tecnologia is connected!' -ForegroundColor DarkCyan -ErrorAction SilentlyContinue
+    } else {
+
+        Write-Host 'Connecting to the VPN VPN_Wiser_Tecnologia' -ForegroundColor Yellow -ErrorAction SilentlyContinue
+
+        rasdial.exe $name $user $password | Out-Null
+
+        if ($vpn.ConnectionStatus -eq 'Connected') { 
+            Write-Host 'VPN VPN_Wiser_Tecnologia is connected!' -ForegroundColor Green -ErrorAction SilentlyContinue
+        } else {
+            Write-Host 'Unable to connect to the VPN VPN_Wiser_Tecnologia' -ForegroundColor Red -ErrorAction SilentlyContinue
+        }
+
+    }
+
+    Start-Sleep -Seconds 3600 -ErrorAction SilentlyContinue
+
+}
