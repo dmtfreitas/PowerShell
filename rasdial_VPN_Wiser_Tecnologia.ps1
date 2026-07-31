fe405711f09989b8cef467438723eb79276c80e3
Demétrio de Freitas Oliveira <81397961+dmtfreitas@users.noreply.github.com>
2026-07-31 11:13:10 -0300

    Update rasdial_VPN_Wiser_Tecnologia.ps1

--- rasdial_VPN_Wiser_Tecnologia.ps1
+++ rasdial_VPN_Wiser_Tecnologia.ps1
@@ -1,11 +1,11 @@
 
 # rasdial_VPN_Wiser_Tecnologia.ps1
 
-$name = "VPN_Wiser_Tecnologia"
-$user = ""
-$password = ""
+$name = 'VPN_Wiser_Tecnologia'
+$user = ''
+$password = ''
 
-function Test-VpnConnected {
+function Get-VpnTestConnected {
 
     param([string]$VpnName)
 
@@ -26,6 +26,7 @@ function Test-VpnConnected {
     }
 
     return $false
+
 }
 
 $exists_connection = Get-VpnConnection -Name $name -ErrorAction SilentlyContinue
@@ -35,30 +36,40 @@ if (-not $exists_connection) {
 }
 
 if (-not $exists_connection) {
+
     Write-Host "VPN $name not found!" -ForegroundColor DarkRed
     Exit
+
 }
 
 while ($true) {
 
-    $timestamp = Get-Date -Format "dd/MM/yyyy HH:mm:ss"
+    $timestamp = Get-Date -Format 'dd/MM/yyyy HH:mm:ss'
 
-    if (Test-VpnConnected -VpnName $name) {
+    if (Get-VpnTestConnected -VpnName $name) {
+
         Write-Host "[$timestamp] VPN $name is connected!" -ForegroundColor DarkCyan
+
     } else {
 
-        Write-Host "[$timestamp] Connecting to the VPN $name" -ForegroundColor Yellow
+        Write-Host "[$timestamp] Connecting to the VPN $name..." -ForegroundColor Yellow
 
         rasdial.exe $name $user $password | Out-Null
 
         Start-Sleep -Seconds 10.5
 
-        if (Test-VpnConnected -VpnName $name) {
+        if (Get-VpnTestConnected -VpnName $name) {
+
             Write-Host "[$timestamp] VPN $name is connected!" -ForegroundColor Green
+
         } else {
+
             Write-Host "[$timestamp] Unable to connect to the VPN $name" -ForegroundColor Red
+
         }
+
     }
 
     Start-Sleep -Seconds 60.5
+
 }
